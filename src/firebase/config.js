09c919113738a317cfff9bc62ef0768abbf400c5
Никита Дурнев <nikitadurnev@Nikitas-Air.lan>
2023-01-13 00:33:19 +0200

    fix: env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,19 @@
-import Config from "react-native-config";
+import { FB_API_KEY, FB_APP_ID } from "@env";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-// import "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(FB_API_KEY);
+
 const firebaseConfig = {
-  apiKey: Config.FB_API_KEY,
+  apiKey: FB_API_KEY,
   authDomain: "react-native-social-a2505.firebaseapp.com",
   projectId: "react-native-social-a2505",
   storageBucket: "react-native-social-a2505.appspot.com",
   messagingSenderId: "714324893287",
-  appId: Config.FB_APP_ID,
+  appId: FB_APP_ID,
   measurementId: "G-89PM0T0598",
 };
 
